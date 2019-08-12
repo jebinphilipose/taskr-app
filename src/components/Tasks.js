4791e0e0d94964679149ccb5dbd4b1ae4b2ac76c
Jebin Philipose <jebinphilip24@gmail.com>
2019-08-13 02:29:19 +0530

    Add text to show when there are no tasks

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -3,6 +3,7 @@ import Task from './Task';
 
 const Tasks = (props) => (
   <div>
+    {props.tasks.length === 0 && <p>Yay, you have no pending tasks!</p>}
     {
       props.tasks.map((task) => <Task key={task} taskText={task} />)
     }
