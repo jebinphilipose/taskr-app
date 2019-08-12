89c9e4ccacb24af9bd2c2694db9bc9ad65c0400b
Jebin Philipose <jebinphilip24@gmail.com>
2019-08-13 01:33:53 +0530

    Add Header Component

--- src/components/TaskrApp.js
+++ src/components/TaskrApp.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Header from './Header';
 
 export default class TaskrApp extends React.Component {
   render() {
+    const subtitle = 'Remember everything important';
+
     return (
-      <div>
-        <p>This is JSX from TaskrApp!</p>
-      </div>
+      <Header subtitle={subtitle} />
     );
   }
 }
