904d17a270633fd347577cd4a4d5efd8fc737644
Jebin Philipose <jebinphilip24@gmail.com>
2019-08-14 01:52:14 +0530

    Add styling for modal

--- src/components/TaskModal.js
+++ src/components/TaskModal.js
@@ -4,10 +4,10 @@ import Modal from 'react-modal';
 Modal.setAppElement('#app');
 
 const TaskModal = (props) => (
-  <Modal isOpen={!!props.selectedTask} contentLabel="Selected Task" onRequestClose={props.handleCloseModal}>
-    <h3>Selected Task</h3>
-    <p>{props.selectedTask}</p>
-    <button onClick={props.handleCloseModal}>Okay</button>
+  <Modal className="modal" isOpen={!!props.selectedTask} contentLabel="Selected Task" onRequestClose={props.handleCloseModal} closeTimeoutMS={200}>
+    <h3 className="modal__title">Selected Task</h3>
+    <p className="modal__body">{props.selectedTask}</p>
+    <button className="button" onClick={props.handleCloseModal}>Okay</button>
   </Modal> 
 );
 
