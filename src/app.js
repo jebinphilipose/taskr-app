4769e32e342cb07f92bf747a3263116d7c202282
Jebin Philipose <jebinphilip24@gmail.com>
2019-08-13 16:58:03 +0530

    Add base styles

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TaskrApp from './components/TaskrApp';
+import './styles/styles.scss';
 
 ReactDOM.render(<TaskrApp />, document.getElementById('app'));
