2d8850ab22ec224be490b960c5b8edbe5733c29d
Jebin Philipose <jebinphilip24@gmail.com>
2019-08-14 00:35:00 +0530

    Add styling for individual task

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const Task = (props) => (
-  <div>
-    {props.taskText}
+  <div className="task">
+    <p className="task__text">{props.taskText}</p>
     <button className="button button--link" onClick={(e) => { props.handleDeleteTask(props.taskText); }}>Remove</button>
   </div>
 );
